0ea7439d024964cc279d4eed62848046d9798bc2
Dmytro Melnyk <dmelnyk@student.42london.com>
2025-05-15 16:08:54 +0100

    fix: Prototype of the mouse handlers takes more parameters

--- src/window/events_mouse.c
+++ src/window/events_mouse.c
@@ -1,13 +1,18 @@
 #include "window_private.h"
 
-int mlx_handle_mouse_press(t_cube *cube)
+int mlx_handle_mouse_press(int key, int x, int y, t_cube *cube)
 {
 	(void) cube;
+	(void) key;
+	(void) x;
+	(void) y;
 	return (0);
 }
 
-int mlx_handle_mouse_move(t_cube *cube)
+int mlx_handle_mouse_move(int x, int y, t_cube *cube)
 {
 	(void) cube;
+	(void) x;
+	(void) y;
 	return (0);
 }
