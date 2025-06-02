d4125d4ae7bc8bd345a788f6ea81fab58f8c4b77
Oleh Hrytsiv <g.r.i.t.s.i.v.o.l.e.g.95@gmail.com>
2025-06-02 11:33:29 +0100

    fix: error messages for all types of error with parser

--- src/utils/check_world.c
+++ src/utils/check_world.c
@@ -6,7 +6,7 @@
 /*   By: ogrativ <ogrativ@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 15:22:42 by ogrativ           #+#    #+#             */
-/*   Updated: 2025/05/28 16:10:28 by ogrativ          ###   ########.fr       */
+/*   Updated: 2025/06/02 11:30:21 by ogrativ          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,22 +26,58 @@ static bool	check_texture(t_image *img, char *texture)
 	return (1);
 }
 
+static bool	check_colors(t_world *world)
+{
+	int	status;
+
+	status = 0;
+	if (world->c == -1)
+	{
+		ft_putstr_fd(RED "Error" RESET ": Ceiling color not found\n",
+			STDERR_FILENO);
+		status++;
+	}
+	if (world->f == -1)
+	{
+		ft_putstr_fd(RED "Error" RESET ": Floor color not found\n",
+			STDERR_FILENO);
+		status++;
+	}
+	if (status > 0)
+		return (0);
+	return (1);
+}
+
+static bool	check_textures(t_world *world)
+{
+	int	status;
+
+	status = 0;
+	status += check_texture(&world->ea, "EA");
+	status += check_texture(&world->we, "WE");
+	status += check_texture(&world->so, "SO");
+	status += check_texture(&world->no, "NO");
+	if (status != 4)
+		return (0);
+	return (1);
+}
+
 bool	check_world(t_world *world)
 {
+	int	error_count;
+
+	error_count = 0;
 	if (!world)
 		return (false);
-	if (world->c == -1 || world->f == -1)
-	{
-		ft_putstr_fd(RED "Error" RESET ": Color not found\n", STDERR_FILENO);
-		return (0);
-	}
+	if (!check_colors(world))
+		error_count++;
 	if (!world->map)
 	{
 		ft_putstr_fd(RED "Error" RESET ": Map not found\n", STDERR_FILENO);
-		return (0);
+		error_count++;
 	}
-	if (!check_texture(&world->ea, "EA") || !check_texture(&world->we, "WE")
-		|| !check_texture(&world->so, "SO") || !check_texture(&world->no, "NO"))
+	if (check_textures(world) && !error_count)
+		return (1);
+	else
 		return (0);
-	return (1);
 }
