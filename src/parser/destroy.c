00d472e97ff642c49c9a8bc7941d37313fc380d6
Oleh Hrytsiv <g.r.i.t.s.i.v.o.l.e.g.95@gmail.com>
2025-06-16 15:29:19 +0100

    fix: memory leaks

--- src/parser/destroy.c
+++ src/parser/destroy.c
@@ -6,7 +6,7 @@
 /*   By: ogrativ <ogrativ@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 15:50:24 by ogrativ           #+#    #+#             */
-/*   Updated: 2025/06/16 12:54:57 by ogrativ          ###   ########.fr       */
+/*   Updated: 2025/06/16 15:26:28 by ogrativ          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,12 @@ void	destroy_textures(void *mlx_ptr, t_textures *textures)
 	while (textures->tp.textures_arr[i] && textures->tp.textures_arr[i]->img)
 		buffer_destroy(mlx_ptr, textures->tp.textures_arr[i++]);
 	i = 0;
-	while (textures->tp.sprites[i] && textures->tp.sprites[i]->img_arr)
+	while (textures->tp.sprites[i])
 	{
 		j = 0;
 		while (j < textures->tp.sprites[i]->len)
 		{
 			buffer_destroy(mlx_ptr, &textures->tp.sprites[i]->img_arr[j]);
-			free(&textures->tp.sprites[i]->img_arr[j].path);
 			j++;
 		}
 		free(textures->tp.sprites[i]->img_arr);
