5eb721892cb678d1226c84c6293a658e05d6bf7b
Dmytro Melnyk <dmelnyk@student.42london.com>
2025-06-17 11:19:40 +0100

    fix: Sprite array start at 0, and on map walls starts from 1

--- src/raycaster/set_wall_properties.c
+++ src/raycaster/set_wall_properties.c
@@ -9,7 +9,7 @@ MAYBE_INLINE void	ray_set_wall_prop(t_engine *eng, t_ray *ray)
 		ray->wall_hit = eng->camera.pos.x + ray->wall_dist * ray->direction.x;
 	ray->wall_hit -= floor(ray->wall_hit);
 	ray->texture = sprite_get_img(&eng->textures.walls,
-		map_get(eng->map, ray->map.x, ray->map.y));
+		map_get(eng->map, ray->map.x, ray->map.y) - 1);
 	ray->x_on_tex = (int)(ray->wall_hit * (double)ray->texture->width);
     if ((ray->side == 0 && ray->direction.x < 0) || (ray->side == 1 && ray->direction.y > 0))
         ray->x_on_tex = ray->texture->width - ray->x_on_tex - 1;
