0236b9c722389e35b07be80f59a0fee9a6856eac
Oleh Hrytsiv <g.r.i.t.s.i.v.o.l.e.g.95@gmail.com>
2025-06-30 14:40:49 +0100

    update: deleted debug line

--- src/world/parse_textures.c
+++ src/world/parse_textures.c
@@ -6,7 +6,7 @@
 /*   By: ogrativ <ogrativ@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 12:15:54 by ogrativ           #+#    #+#             */
-/*   Updated: 2025/06/16 16:15:02 by ogrativ          ###   ########.fr       */
+/*   Updated: 2025/06/30 14:30:12 by ogrativ          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,7 +98,6 @@ static int	parse_texture(void *mlx_ptr, t_world *world, t_string *str)
 		return (-1);
 	}
 	space_idx = assigne_texture(mlx_ptr, world, path, tex_name);
-	printf("texture: %s   PATH: %s\n", tex_name, path);
 	free(tex_name);
 	return (space_idx);
 }
